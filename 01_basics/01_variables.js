20985068e646613bf3b89829cc40f26c19f39ac4
sabeel9621 <159629226+sabeel9621@users.noreply.github.com>
2024-08-31 17:24:10 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId=12345
+let accountEmail="sabeel@gmail.com"
+var accountPassword="12121"
+accountCity="jaipur"
+let accountState;
+
+//accountId=2 // redeaclaration and reasignment not allowd
+
+accountEmail="gtr@jdgmail.com"
+accountPassword="11224455"
+accountCity="bangaluru"
+
+console.log(accountId);
+
+/*
+prefer not to use var 
+becouse of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
