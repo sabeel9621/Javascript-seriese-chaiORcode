9de1823d9afe4cc93912f8a865b7b4b06c84516d
sabeel9621 <159629226+sabeel9621@users.noreply.github.com>
2024-09-03 17:31:01 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score=true
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//"33"=>33
+//"33abc"=>Nan
+//true=>1; false=>0
+
+let isLoggedin="sabeel"
+
+let booleanIsLoggedin=Boolean(isLoggedin)
+// console.log(booleanIsLoggedin)
+
+//1=>true; 0=> false
+//""=> false
+//"sabeel" => true
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+    console.log(stringNumber);
+    console.log(typeof stringNumber);
